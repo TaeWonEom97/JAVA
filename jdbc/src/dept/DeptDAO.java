ed593be2d689d9f97fba9263bcd64d72700b5eb3
won <utw1960@naver.com>
2021-09-30 15:59:21 +0900

    DAO

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,28 @@
 package dept;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DeptDAO {
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@EOMTAEWON:1522:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
